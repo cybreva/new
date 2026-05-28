0bd23965bc4cb492b827c53934cc2f5770f4d9e5
Tanmay <example@gmail.com>
2026-05-28 17:09:25 +0530

    imp

--- main.c
+++ main.c
@@ -22,7 +22,17 @@ int main()
     long double myfloat3 = 2.4837589023;
     unsigned integer3 = 2352;
     unsigned long integer4 = 2345542 ;
-    printf("the size of integer is %ld \n " , sizeof(integer));
-    printf("hello world");     
+    printf("the size of integer is %d \n " , sizeof(a));
+    printf("the size of float is %f \n " , sizeof(b));
+    printf("the size of char is %c \n " , sizeof(c));
+    printf("the size of unsigned integer is %u \n " , sizeof(integer3));
+    printf("the size of unsigned long integer is %lu \n " , sizeof(integer4));
+    printf("the size of long integer is %ld \n " , sizeof(integer));
+    printf("the size of short integer is %ld \n " , sizeof(integer2));
+    printf("the size of double float is %lf \n " , sizeof(myfloat));
+    printf("the size of long double integer is %Lf \n " , sizeof(myfloat3));
+    
+
+    
     return 0;
 }
