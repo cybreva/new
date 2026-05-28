8a7df287aac23c1746050cfede324fdb7a2193ec
Tanmay <example@gmail.com>
2026-05-28 17:52:57 +0530

    logic building

--- main.c
+++ main.c
@@ -1,28 +1,52 @@
 #include <stdio.h>
 
-int main(){
-
+int main()
+{
     int num1;
     int num2;
-    printf("enter first number ");
-    scanf("%d",&num1);
 
-    printf("enter second number ");
-    scanf("%d",&num2);
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+
+    printf("Enter second number: ");
+    scanf("%d", &num2);
 
     printf("\n");
 
-    printf("addition: %d \n" , num1+num2);
-    printf("subtraction: %d \n" , num1-num2);
-    printf("multiplication: %d \n" , num1*num2);
-    printf("division: %d \n" , num1/num2);
+    printf("a = addition\n");
+    printf("b = subtraction\n");
+    printf("c = multiplication\n");
+    printf("d = division\n");
 
+    char z;
 
+    printf("Enter your option: ");
+    scanf(" %c", &z);
 
-  
+    if(z == 'a')
+    {
+        printf("Addition: %d\n", num1 + num2);
+    }
 
-    return 0 ;
+    else if(z == 'b')
+    {
+        printf("Subtraction: %d\n", num1 - num2);
+    }
 
-    
+    else if(z == 'c')
+    {
+        printf("Multiplication: %d\n", num1 * num2);
+    }
 
+    else if(z == 'd')
+    {
+        printf("Division: %d\n", num1 / num2);
+    }
+
+    else
+    {
+        printf("Invalid option\n");
+    }
+
+    return 0;
 }
