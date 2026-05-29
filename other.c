dfeb24cba66e6381cbd8ac33a86d4867f8c68141
Tanmay <example@gmail.com>
2026-05-29 16:25:45 +0530

    number addition logic

---
+++ other.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main (){
+    char c ;
+    printf("Enter your name initial ; \n ");
+    scanf("%c", &c);
+    
+    if(c=='t' || c== 'T')
+    {
+
+
+        int n ;
+        printf("how many numbers you want to add ?\n");
+        scanf("%d", &n);
+
+        int i = 1 ;
+        int num;
+        int sum = 0 ;
+
+
+        while (i<=n){
+
+            printf("enter number %d\n",i);
+            scanf("%d", &num);
+            i++;
+            
+            sum = sum + num ;
+            
+    } 
+         printf("Total sum = %d\n", sum);
+
+    }
+    
+    else{
+        printf("sorry you are not allowed");
+    }
+    
+    return 0;
+}
