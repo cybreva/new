8e961a0432fe6da71733f97f4f982677dbd79415
Tanmay <example@gmail.com>
2026-06-04 19:30:09 +0530

    understanding pointers conceptcd

---
+++ pointers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int x = 10;
+
+    int *ptr = &x;
+
+    printf("\nx = %d\n", x);
+    printf("\nptr = %p\n", ptr);
+    printf("\n*ptr = %d\n", *ptr);
+    printf("by by ")
+
+
+    return 0;
+}
